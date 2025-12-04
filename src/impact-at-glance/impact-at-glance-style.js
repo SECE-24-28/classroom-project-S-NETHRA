806d0fbd8e8d1d99a6049b35f1b2374ace492d25
S-NETHRA <nethra.s2024cse@sece.ac.in>
2025-12-04 23:06:53 +0530

    day 4 task in React

---
+++ src/impact-at-glance/impact-at-glance-style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const GlanceWrapper = styled.div`
+  padding: 60px 20px;
+  text-align: center;
+  font-family: "Inter", sans-serif;
+
+  h1 {
+    font-size: 42px;
+    font-weight: 700;
+    margin-bottom: 30px;
+  }
+
+  span.red {
+    color: #ff3b47;
+  }
+`;
